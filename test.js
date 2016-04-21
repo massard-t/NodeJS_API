c0cf3a8d8a710642dcea8cf71c7210f34d351a60
robotbobtm <massar_t@etna-alternance.net>
2016-04-21 13:15:08 +0000

    [ON]check diff heures

--- test.js
+++ test.js
@@ -1,12 +1,13 @@
-var mysql = require("mysql");
+var moment = require('moment');
 
-function DefineDB() {
-    const connection = mysql.createConnection({
-        host     : '0.0.0.0',
-        user     : 'robotbobtm',
-        database : 'asptt'
-    });
-    connection.connect();
-    return (connection);
+var date2 = "2015-07-16T16:33:39.113Z"
+
+var then = moment(date2, "YYYY-MM-DD'T'HH:mm:ss:SSSZ");
+var now = moment();
+
+var diff = moment.duration(then.diff(now));
+if (diff < 0) {
+    diff = Math.abs(diff);
 }
-
+var d = moment.utc(diff).format("HH:mm:ss");
+console.log("Difference: " + d);
